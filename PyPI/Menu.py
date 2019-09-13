8cbf03c6113ed8df5b726d68474598ef11b2082b
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-13 13:25:53 +0700

    Update Menu.py

--- PyPI/Menu.py
+++ PyPI/Menu.py
@@ -2,25 +2,19 @@
 def show_menu():
     print "\n"
     print "----------- MENU ----------"
-    print "[1] Show Data"
-    print "[2] Insert Data"
-    print "[3] Edit Data"
-    print "[4] Delete Data"
-    print "[5] Exit"
+    print "[1] Login"
+    print "[2] About"
+    print "[3] Exit"
     
     menu = input("PILIH MENU> ")
     print "\n"
 
     if menu == 1:
-        show_data()
+        login_data()
     elif menu == 2:
-        insert_data()
+        about_data()
     elif menu == 3:
-        edit_data()
-    elif menu == 4:
-        delete_data()
-    elif menu == 5:
-        exit()
+        exit_data()
     else:
         print "Salah pilih!"
 
