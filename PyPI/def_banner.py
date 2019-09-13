83afacece1c6147deb341a44c974000d2c445d1e
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-13 19:52:33 +0700

    Create def_banner.py

---
+++ PyPI/def_banner.py
@@ -0,0 +1,13 @@
+def banner():
+	os.system("clear")
+	print("""
+╭═══════════════════════════╮
+%s[+]%s= %sSAN-Brother Auto Crack V.1.0.1%s =%s[+]%s
+╰═══════════════════════════╯
+%s║ %sAuthor : %sSusanto%s %s                      
+%s║ %sFB     : %sHttps://fb.me/san.brother%s  %s     
+%s║ %sGithub : %sHttps://github.com/SAN-Brother%s%s 
+╭═══════════════════════════╮
+╰═══════════════════════════╯
+ """%(R,G,GB,RE,R,G,R,W,RM,RE,W,R,W,CM,RE,W,R,W,PM,RE,W))
+cek()
